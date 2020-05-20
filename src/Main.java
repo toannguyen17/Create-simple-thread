e1d94019917fdb9e7d61a25810b6006a86a31be3
MrT98 <toan.vp98.wapmaster@gmail.com>
2020-05-20 14:53:05 +0700

    add project

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         //NumberGenerator num = new NumberGenerator();
-        new Thread(new NumberGenerator()).start();
+        Thread threadA = new Thread(new NumberGenerator("A"));
+        Thread threadB = new Thread(new NumberGenerator("B"));
+
+        threadA.setPriority(threadA.MIN_PRIORITY);
+        threadB.setPriority(threadA.MAX_PRIORITY);
+
+        threadA.start();
+        threadB.start();
     }
 }
--- src/NumberGenerator.java
+++ src/NumberGenerator.java
@@ -1,7 +1,13 @@
 public class NumberGenerator implements Runnable {
+    String name;
     int numMax;
     int count;
     NumberGenerator(){
+        numMax = 10;
+        count  = 0;
+    }
+    NumberGenerator(String name){
+        this.name = name;
         numMax = 10;
         count  = 0;
     }
@@ -9,7 +15,7 @@ public class NumberGenerator implements Runnable {
     public void run() {
         while (count < numMax){
             count++;
-            System.out.println(count + " - " + this.hashCode());
+            System.out.println(this.name + " ___ " + count + " - " + this.hashCode());
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
